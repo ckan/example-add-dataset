71cc6d70b8669305f152e451b2333c3f5c0aa376
amercader <amercadero@gmail.com>
2014-07-22 14:51:44 +0100

    Pass url when creating a resource
    It is needed even if it is a direct upload, otherwise you get a
    Validation error.
    
    Also print errors from the API, otherwise is hard to know what is going
    on.
    
    Some PEP8 as well.

--- add_example_datasets.py
+++ add_example_datasets.py
@@ -15,10 +15,10 @@ package_title = 'Data viewer examples'
 try:
     print 'Creating "{package_title}" package'.format(**locals())
     package = ckan.action.package_create(name=package_name,
-                                        title=package_title)
+                                         title=package_title)
 except ckanapi.ValidationError, e:
     if (e.error_dict['__type'] == 'Validation Error' and
-        e.error_dict['name'] == ['That URL is already in use.']):
+       e.error_dict['name'] == ['That URL is already in use.']):
         print '"{package_title}" package already exists'.format(**locals())
         package = ckan.action.package_show(id=package_name)
     else:
@@ -29,10 +29,15 @@ for filename in os.listdir('example_files'):
     extension = os.path.splitext(filename)[1][1:].upper()
     resource_name = 'Example {extension} file'.format(extension=extension)
     print 'Creating "{resource_name}" resource'.format(**locals())
-    requests.post('http://127.0.0.1:5000/api/action/resource_create',
-                  data={'package_id': package['id'],
-                        'name': resource_name,
-                        'format': extension,
-                        },
-                  headers={'Authorization': APIKEY},
-                  files=[('upload', file(path))])
+    r = requests.post('http://127.0.0.1:5001/api/action/resource_create',
+                      data={'package_id': package['id'],
+                            'name': resource_name,
+                            'format': extension,
+                            'url': 'upload',  # Needed to pass validation
+                            },
+                      headers={'Authorization': APIKEY},
+                      files=[('upload', file(path))])
+
+    if r.status_code != 200:
+        print 'Error while creating resource: {0}'.format(r.content)
+        break
